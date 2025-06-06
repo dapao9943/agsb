96e332ad1935d742e6f39270963f945fa5822a25
dapao9943 <162852074+dapao9943@users.noreply.github.com>
2025-06-06 15:45:54 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "dapao"         # 用户名
+UUID = "f350291d-d741-42a8-aa42-222b19c9560c"                     # UUID，留空则自动生成
+PORT = 443                  # Vmess端口，留空或0则自动生成
+DOMAIN = "stream.9943826.xyz"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYTIyMWM5NDJhZTBjNDg0ZDNhMmZjNTM4NDI5ODMxZDQiLCJ0IjoiOTI0NmRhNTYtYzQ4Zi00OTIyLTkwNmMtZTk4ZjE3N2QwNmU3IiwicyI6Ik5qbGxOVE5rWTJNdE9HSmhOQzAwTkdFekxXSTNZall0WmpneU5tVTNNekJqTkdZeCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
